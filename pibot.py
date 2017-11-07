b18e49c0c852218dfc84a3d7d9937f7d48733f70
Arnau Bennassar Formenti <arnaubennassar5@gmail.com>
2017-11-08 00:17:40 +0100

    first script 0.1

---
+++ pibot.py
@@ -0,0 +1,21 @@
+import telepot
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print 'Got command: %s' % command
+
+    if command == '/hi':
+        bot.sendMessage(chat_id, 'hello world motherfucker')
+    else
+        bot.sendMessage(chat_id, 'say what nigga')
+
+bot = telepot.Bot('*')
+
+MessageLoop(bot, handle).run_as_thread()
+print 'I am listening ...'
+
+while 1:
+    time.sleep(10)
